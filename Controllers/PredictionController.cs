27497956f3a046ff726a67d6dcc72f73b15b512e
İbrahim Tuğrul <sayi.krali@hotmail.com>
2024-11-16 16:39:32 +0300

    .

--- Controllers/PredictionController.cs
+++ Controllers/PredictionController.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RentPricePredictionWeb.Models;
 
 namespace RentPricePredictionWeb.Controllers
 {
     public class PredictionController : Controller
     {
-        public IActionResult Prediction()
+        public IActionResult PredictionPage()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Prediction(House house)
+        {
+            return Json(..);
         }
     }
 }
---
+++ Models/House.cs
@@ -0,0 +1,11 @@
+﻿namespace RentPricePredictionWeb.Models
+{
+    public class House
+    {
+        public int Price { get; set; }
+        public string? HouseType { get; set; }
+        public string? Location { get; set; }
+        public string? Floor { get; set; }
+        public int Area { get; set; }
+    }
+}
